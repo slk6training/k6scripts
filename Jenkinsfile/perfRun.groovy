3d09d81f9c10ce9f889e51d4f7e1ba3a6d038993
slk6training <128468860+slk6training@users.noreply.github.com>
2023-04-20 15:40:41 +0530

    Update perfRun.groovy

--- Jenkinsfile/perfRun.groovy
+++ Jenkinsfile/perfRun.groovy
@@ -14,6 +14,7 @@ pipeline {
     environment {
         PURPOSE="LOAD-TEST"
         LG_COUNT="1"
+        SERVICE_ID="ps"
         k6_DOCKER_IMAGE="loadimpact/k6:0.30.0"
     }
     stages {
@@ -21,30 +22,30 @@ pipeline {
             steps {
                 script {
                     echo 'Hello world!'
-                   // loadScripts()
+                    loadScripts()
                    // commonScripts.cancelPreviousBuilds()
                 }
             }
         }
-        // stage('Setup') {
-        //     steps {
-        //         dir('performance') {
-        //            sh 'yarn k6:setup'
-        //         }
-        //         sh script: 'sudo chown -R $USER:$(id -g -n) .', label: "Set file system ownership"
-        //         sh script: 'sudo chmod -R 775 .', label: "Set file system permissions"
-        //     }
-        // }
-        // stage('Test') {
-        //     steps {
-        //         sh script: "docker run --name k6-docker-${env.SERVICE_ID} --rm -v $WORKSPACE/performance:/perf -w /perf --ipc host --network host -p 8125:8125/udp -e RAMP_UP=${RAMP_UP} -e HOLD_DURATION=${HOLD_DURATION} -e TEAR_DOWN=${TEAR_DOWN} -e TARGET_LOAD=${TARGET_LOAD} -i ${env.k6_DOCKER_IMAGE} --out datadog --tag test_run_id=${env.SERVICE_ID}_${currentBuild.number} --quiet run main.js", label: "Run perf test"
-        //     }
-        //     post {
-        //         always {
-        //             sh script: "docker rm -f \$(docker ps --filter name=k6-docker-${env.SERVICE_ID} --format {{.Names}}) || true", label: "Remove k6 docker container"
-        //         }
-        //     }
-        // }
+        stage('Setup') {
+            steps {
+                dir('performance') {
+                   sh 'yarn k6:setup'
+                }
+                sh script: 'sudo chown -R $USER:$(id -g -n) .', label: "Set file system ownership"
+                sh script: 'sudo chmod -R 775 .', label: "Set file system permissions"
+            }
+        }
+        stage('Test') {
+            steps {
+                sh script: "docker run --name k6-docker-${env.SERVICE_ID} --rm -v $WORKSPACE/performance:/perf -w /perf --ipc host --network host -p 8125:8125/udp -e RAMP_UP=${RAMP_UP} -e HOLD_DURATION=${HOLD_DURATION} -e TEAR_DOWN=${TEAR_DOWN} -e TARGET_LOAD=${TARGET_LOAD} -i ${env.k6_DOCKER_IMAGE} --out datadog --tag test_run_id=${env.SERVICE_ID}_${currentBuild.number} --quiet run main.js", label: "Run perf test"
+            }
+            post {
+                always {
+                    sh script: "docker rm -f \$(docker ps --filter name=k6-docker-${env.SERVICE_ID} --format {{.Names}}) || true", label: "Remove k6 docker container"
+                }
+            }
+        }
     }
 }
 
