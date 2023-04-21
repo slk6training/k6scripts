3774871d656811ba1092fed44a6d39316b218d17
slk6training <128468860+slk6training@users.noreply.github.com>
2023-04-22 00:41:26 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-apt-get update
-apt-get install dirmngr --install-recommends
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+sudo apt-get update
+sudo apt-get install dirmngr --install-recommends
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
 echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-apt-get update
-apt-get install k6
+sudo apt-get update
+sudo apt-get install k6
