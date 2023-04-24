67539124ccd617f74915e7f208b7ba05a085c305
slk6training <128468860+slk6training@users.noreply.github.com>
2023-04-24 14:53:53 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -3,17 +3,13 @@ set -ex
 
 #!/bin/bash
 
-# Define the repository name and owner
-repository_owner="example_owner"
-repository_name="example_repo"
-
 # Use the GitHub API to get the latest release version
-latest_release=$(curl -s https://api.github.com/repos/${repository_owner}/${repository_name}/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+latest_release=$(curl -s https://api.github.com/repos/grafana/k6/releases/latest | grep "v0.43.1" | cut -d '"' -f 4)
 
 # Download the release zip file
-wget https://github.com/${repository_owner}/${repository_name}/archive/${latest_release}.zip -O ${latest_release}.zip
+wget https://github.com/grafana/k6/archive/k6-v0.43.1-macos-arm64.zip -O k6-v0.43.1-macos-arm64.zip
 
-echo "${password}" | sudo -S apt-get update
+sudo -S apt-get update
 sudo apt-get install dirmngr --install-recommends
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
 echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
