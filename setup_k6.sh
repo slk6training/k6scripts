3227ebad8abd9d8854a03b02786d08e01e1116ac
slk6training <128468860+slk6training@users.noreply.github.com>
2023-04-24 12:18:04 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -2,7 +2,7 @@
 set -ex
 
 password="sahani@123"
-echo "${password}" | sudo - S apt-get update
+echo "${password}" | sudo -S apt-get update
 sudo apt-get install dirmngr --install-recommends
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
 echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
